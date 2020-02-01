871e26100150a8487fe82d53c65ba25dcfd2fe6b
Berk Emre Saribas <berksaribas@gmail.com>
2020-02-01 12:04:53 +0300

    Implement FPS camera and player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody PlayerRigidbody;
+
+    void Awake()
     {
-        
     }
 
     // Update is called once per frame
